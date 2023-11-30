5c6b959e7cf20bbab123b4142b0d0bcc88dd36c6
MohammedAbdulaziz <mohamed.abaziz980@gmail.com>
2023-11-30 17:31:44 +0200

    patient login

--- src/pages/PatientSignup.jsx
+++ src/pages/PatientSignup.jsx
@@ -1,5 +1,88 @@
 import React from "react";
+import {
+  TextField,
+  Button,
+  Grid,
+  Typography,
+  Container,
+} from "@mui/material";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 export const PatientSignup = () => {
-  return <div>PatientSignup</div>;
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="h4" align="center" gutterBottom>
+        Patient Signup
+      </Typography>
+      <form>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              label="First Name"
+              name="firstName"
+              required
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField fullWidth label="Last Name" name="lastName" required />
+          </Grid>
+          <Grid item xs={12}>
+            <Grid item xs={12}>
+              <Button
+                variant="contained"
+                component="label"
+                fullWidth
+                startIcon={<CloudUploadIcon />}
+              >
+                Upload Photo
+                <input type="file" hidden />
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              label="Mobile Number"
+              name="mobileNumber"
+              required
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField fullWidth label="Email" name="email" required />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              label="National ID"
+              name="nationalId"
+              required
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              label="Date of Birth"
+              name="dateOfBirth"
+              type="date"
+              required
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+            >
+              Sign Up
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
 };
