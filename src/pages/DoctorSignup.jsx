044796b18e99a58c70dba9f214cc21767cc101fe
stevensamir20 <steven.samir@eng.suez.edu.eg>
2023-12-01 00:40:42 +0200

    doctor signup

--- src/pages/DoctorSignup.jsx
+++ src/pages/DoctorSignup.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   TextField,
   Button,
@@ -8,105 +7,134 @@ import {
   MenuItem,
 } from "@mui/material";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import { LoginContainer } from "../components/LoginContainer";
 
 export const DoctorSignup = () => {
   return (
-    <Container maxWidth="sm">
-      <Typography variant="h4" align="center" gutterBottom>
-        Doctor Signup
-      </Typography>
-      <form>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              fullWidth
-              label="First Name"
-              name="firstName"
-              required
-            />
+    <LoginContainer>
+      <Container maxWidth="sm">
+        <Typography
+          variant="h4"
+          align="center"
+          gutterBottom
+          sx={{
+            marginBottom: "2rem",
+          }}
+        >
+          Create an Account as a Doctor
+        </Typography>
+        <form>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label="First Name"
+                name="firstName"
+                required
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField fullWidth label="Last Name" name="lastName" required />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                variant="contained"
+                component="label"
+                fullWidth
+                startIcon={<CloudUploadIcon />}
+              >
+                Upload Photo
+                <input type="file" hidden />
+              </Button>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label="Clinic Phone Number"
+                name="clinicPhoneNumber"
+                required
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label="Clinic Email"
+                name="clinicEmail"
+                required
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label="Date of Birth"
+                name="dateOfBirth"
+                type="date"
+                required
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label="Specialization"
+                name="specialization"
+                select
+                required
+              >
+                <MenuItem value="Cardiologist">Cardiologist</MenuItem>
+                <MenuItem value="Dermatologist">Dermatologist</MenuItem>
+                {/* Add more specialization options as needed */}
+              </TextField>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label="Clinic Address"
+                name="clinicAddress"
+                required
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label="Workplace"
+                name="workplace"
+                required
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label="Fees"
+                name="fees"
+                type="number"
+                required
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                label="Password"
+                name="password"
+                type="password"
+                required
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                fullWidth
+              >
+                Sign Up
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField fullWidth label="Last Name" name="lastName" required />
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              variant="contained"
-              component="label"
-              fullWidth
-              startIcon={<CloudUploadIcon />}
-            >
-              Upload Photo
-              <input type="file" hidden />
-            </Button>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              fullWidth
-              label="Clinic Phone Number"
-              name="clinicPhoneNumber"
-              required
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField fullWidth label="Clinic Email" name="clinicEmail" required />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              fullWidth
-              label="Date of Birth"
-              name="dateOfBirth"
-              type="date"
-              required
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              fullWidth
-              label="Specialization"
-              name="specialization"
-              select
-              required
-            >
-              <MenuItem value="Cardiologist">Cardiologist</MenuItem>
-              <MenuItem value="Dermatologist">Dermatologist</MenuItem>
-              {/* Add more specialization options as needed */}
-            </TextField>
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              label="Clinic Address"
-              name="clinicAddress"
-              required
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField fullWidth label="Workplace" name="workplace" required />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              label="Fees"
-              name="fees"
-              type="number"
-              required
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              fullWidth
-            >
-              Sign Up
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
-    </Container>
+        </form>
+      </Container>
+    </LoginContainer>
   );
 };
--- src/pages/PatientSignup.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const PatientSignup = () => {
-  return <div>PatientSignup</div>;
-};
