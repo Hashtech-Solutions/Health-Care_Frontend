46369966e8ef08b64e17d7ae9834c269d1b412cb
MohammedAbdulaziz <mohamed.abaziz980@gmail.com>
2023-12-02 01:11:21 +0200

    fix double footer

--- src/roles/patient/doctors/DoctorsView.jsx
+++ src/roles/patient/doctors/DoctorsView.jsx
@@ -1,14 +1,9 @@
 import { CardGrid } from "../../../components/CardGrid";
 import { MainContainer } from "../../../components/MainContainer";
-import { Footer } from "../../../components/Footer";
-import { Box } from "@mui/material";
 export const DoctorsView = () => {
   return (
-    <Box>
-      <MainContainer title="Doctors">
-        <CardGrid />
-      </MainContainer>
-      <Footer />
-    </Box>
+    <MainContainer title="Doctors">
+      <CardGrid />
+    </MainContainer>
   );
 };
