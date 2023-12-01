9778c2f099064ca4006d6bb311e9b44dad9b90a7
MohammedAbdulaziz <mohamed.abaziz980@gmail.com>
2023-12-02 01:18:54 +0200

    change footer theme

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ export function Footer() {
     <Box
       component="footer"
       sx={{
-        backgroundColor: "lightblue",
+        backgroundColor: "var(--primary)",
         p: 6,
         width: "100%",
         boxSizing: "border-box",
@@ -20,45 +20,45 @@ export function Footer() {
       <Container maxWidth="lg">
         <Grid container spacing={5}>
           <Grid item xs={12} sm={4}>
-            <Typography variant="h6" color="text.primary" gutterBottom>
+            <Typography variant="h6" color="white" gutterBottom>
               About Us
             </Typography>
-            <Typography variant="body2" color="text.secondary">
+            <Typography variant="body2" color="white">
               We are Veezeta, dedicated to providing the best service to our
               customers.
             </Typography>
           </Grid>
           <Grid item xs={12} sm={4}>
-            <Typography variant="h6" color="text.primary" gutterBottom>
+            <Typography variant="h6" color="white" gutterBottom>
               Contact Us
             </Typography>
-            <Typography variant="body2" color="text.secondary">
+            <Typography variant="body2" color="white">
               123 Main Street, Anytown, USA
             </Typography>
-            <Typography variant="body2" color="text.secondary">
+            <Typography variant="body2" color="white">
               Email: info@example.com
             </Typography>
-            <Typography variant="body2" color="text.secondary">
+            <Typography variant="body2" color="white">
               Phone: +1 234 567 8901
             </Typography>
           </Grid>
           <Grid item xs={12} sm={4}>
-            <Typography variant="h6" color="text.primary" gutterBottom>
+            <Typography variant="h6" color="white" gutterBottom>
               Follow Us
             </Typography>
             <Link href="#" color="inherit">
-              <Facebook />
+              <Facebook sx={{ color: "white" }} />
             </Link>
             <Link href="#" color="inherit" sx={{ pl: 1, pr: 1 }}>
-              <Instagram />
+              <Instagram sx={{ color: "white" }} />
             </Link>
             <Link href="#" color="inherit">
-              <Twitter />
+              <Twitter sx={{ color: "white" }} />
             </Link>
           </Grid>
         </Grid>
         <Box mt={5}>
-          <Typography variant="body2" color="text.secondary" align="center">
+          <Typography variant="body2" color="white" align="center">
             {"Copyright © "}
             <Link color="inherit" href="#">
               Veezeta
