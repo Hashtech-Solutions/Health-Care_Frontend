bc896c42d54b534f205877510da2be184d4bf969
stevensamir20 <steven.samir@eng.suez.edu.eg>
2023-12-02 16:09:40 +0200

    categories fix

--- src/roles/patient/portal/CategoriesPortal.jsx
+++ src/roles/patient/portal/CategoriesPortal.jsx
@@ -5,7 +5,7 @@ export const CategoriesPortal = () => {
   return (
     <Container component="section" sx={{ mt: 8, mb: 4 }}>
       <Typography variant="h4" marked="center" align="center" component="h2">
-        Browse all Medical Specilizations
+        Explore all Medical Specializations
       </Typography>
       <CategoriesLayout />
     </Container>
