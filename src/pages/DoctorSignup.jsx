eb2b4fa684432d40d2b7e7ae75f4924d83cb0dd5
stevensamir20 <steven.samir@eng.suez.edu.eg>
2023-12-02 13:22:22 +0200

    sign up axios post request

--- src/pages/DoctorSignup.jsx
+++ src/pages/DoctorSignup.jsx
@@ -1,3 +1,10 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "../shared/API";
+import { Categories } from "../shared/data/Categories";
+
+// Components
 import {
   TextField,
   Button,
@@ -5,144 +12,194 @@ import {
   Typography,
   Container,
   MenuItem,
+  Box,
+  Alert,
 } from "@mui/material";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import { Link } from "react-router-dom";
 import { LoginContainer } from "../components/LoginContainer";
-import { Categories } from "../shared/data/Categories";
+import { BackdropLoader } from "../components/BackdropLoader";
 
 export const DoctorSignup = () => {
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setLoader(true);
+    setError(false);
+    const data = new FormData(event.currentTarget);
+
+    axios
+      .post(`${BASE_URL}/doctor/register`, {
+        firstName: data.get("firstName"),
+        lastName: data.get("lastName"),
+        clinicPhoneNumber: data.get("clinicPhoneNumber"),
+        clinicEmail: data.get("clinicEmail"),
+        dateOfBirth: data.get("dateOfBirth"),
+        specialization: data.get("specialization"),
+        clinicAddress: data.get("clinicAddress"),
+        workplace: data.get("workplace"),
+        fees: data.get("fees"),
+        password: data.get("password"),
+      })
+      .then((res) => {
+        console.log(res);
+        setLoader(false);
+        navigate("/login");
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoader(false);
+        setError(true);
+      });
+  };
+
   return (
-    <LoginContainer>
-      <Container maxWidth="sm">
-        <Typography
-          variant="h4"
-          align="center"
-          gutterBottom
-          sx={{
-            marginBottom: "2rem",
-          }}
-        >
-          Create an Account as a Doctor
-        </Typography>
-        <form>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                fullWidth
-                label="First Name"
-                name="firstName"
-                required
-              />
+    <>
+      {loader && <BackdropLoader />}
+      <LoginContainer>
+        <Container maxWidth="sm">
+          <Typography
+            variant="h4"
+            align="center"
+            gutterBottom
+            sx={{
+              marginBottom: "2rem",
+            }}
+          >
+            Create an Account as a Doctor
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  label="First Name"
+                  name="firstName"
+                  required
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  label="Last Name"
+                  name="lastName"
+                  required
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  variant="contained"
+                  component="label"
+                  fullWidth
+                  startIcon={<CloudUploadIcon />}
+                >
+                  Upload Photo
+                  <input type="file" hidden />
+                </Button>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  label="Clinic Phone Number"
+                  name="clinicPhoneNumber"
+                  required
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  label="Clinic Email"
+                  name="clinicEmail"
+                  required
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  label="Date of Birth"
+                  name="dateOfBirth"
+                  type="date"
+                  required
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  label="Specialization"
+                  name="specialization"
+                  select
+                  required
+                  value={""}
+                >
+                  {Categories.map((category) => (
+                    <MenuItem key={category} value={category.value}>
+                      {category.name}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  label="Clinic Address"
+                  name="clinicAddress"
+                  required
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  label="Workplace"
+                  name="workplace"
+                  required
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  label="Fees"
+                  name="fees"
+                  type="number"
+                  required
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  label="Password"
+                  name="password"
+                  type="password"
+                  required
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                >
+                  Sign Up
+                </Button>
+              </Grid>
+              <Grid item>
+                <Link to="/login">{"Already have an account? Sign In"}</Link>
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField fullWidth label="Last Name" name="lastName" required />
-            </Grid>
-            <Grid item xs={12}>
-              <Button
-                variant="contained"
-                component="label"
-                fullWidth
-                startIcon={<CloudUploadIcon />}
-              >
-                Upload Photo
-                <input type="file" hidden />
-              </Button>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                fullWidth
-                label="Clinic Phone Number"
-                name="clinicPhoneNumber"
-                required
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                fullWidth
-                label="Clinic Email"
-                name="clinicEmail"
-                required
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                fullWidth
-                label="Date of Birth"
-                name="dateOfBirth"
-                type="date"
-                required
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                fullWidth
-                label="Specialization"
-                name="specialization"
-                select
-                required
-                value={""}
-              >
-                {Categories.map((category) => (
-                  <MenuItem key={category} value={category.value}>
-                    {category.name}
-                  </MenuItem>
-                ))}
-              </TextField>
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                label="Clinic Address"
-                name="clinicAddress"
-                required
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                label="Workplace"
-                name="workplace"
-                required
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                label="Fees"
-                name="fees"
-                type="number"
-                required
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                label="Password"
-                name="password"
-                type="password"
-                required
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                fullWidth
-              >
-                Sign Up
-              </Button>
-            </Grid>
-            <Grid item>
-              <Link to="/login">{"Already have an account? Sign In"}</Link>
-            </Grid>
-          </Grid>
-        </form>
-      </Container>
-    </LoginContainer>
+            {error && (
+              <Alert severity="error" sx={{ mt: "20px" }}>
+                Something went wrong please try again later!
+              </Alert>
+            )}
+          </Box>
+        </Container>
+      </LoginContainer>
+    </>
   );
 };
--- src/pages/PatientSignup.jsx
+++ src/pages/PatientSignup.jsx
@@ -1,98 +1,157 @@
-import { TextField, Button, Grid, Typography, Container } from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "../shared/API";
+
+// Components
+import {
+  TextField,
+  Button,
+  Grid,
+  Typography,
+  Container,
+  Alert,
+  Box,
+} from "@mui/material";
 import { LoginContainer } from "../components/LoginContainer";
+import { BackdropLoader } from "../components/BackdropLoader";
 import { Link } from "react-router-dom";
 
 export const PatientSignup = () => {
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setLoader(true);
+    setError(false);
+    const data = new FormData(event.currentTarget);
+    axios
+      .post(`${BASE_URL}/user/register`, {
+        firstName: data.get("firstName"),
+        lastName: data.get("lastName"),
+        mobileNumber: data.get("mobileNumber"),
+        nationalId: data.get("nationalId"),
+        dateOfBirth: data.get("dateOfBirth"),
+        email: data.get("email"),
+        password: data.get("password"),
+      })
+      .then((res) => {
+        console.log(res);
+        setLoader(false);
+        navigate("/login");
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoader(false);
+        setError(true);
+      });
+  };
+
   return (
-    <LoginContainer>
-      <Container maxWidth="sm">
-        <Typography
-          variant="h4"
-          align="center"
-          gutterBottom
-          sx={{
-            marginBottom: "3rem",
-          }}
-        >
-          Create an Account as a Patient
-        </Typography>
-        <form>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                fullWidth
-                label="First Name"
-                name="firstName"
-                required
-              />
+    <>
+      {loader && <BackdropLoader />}
+      <LoginContainer>
+        <Container maxWidth="sm">
+          <Typography
+            variant="h4"
+            align="center"
+            gutterBottom
+            sx={{
+              marginBottom: "3rem",
+            }}
+          >
+            Create an Account as a Patient
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  label="First Name"
+                  name="firstName"
+                  required
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  fullWidth
+                  label="Last Name"
+                  name="lastName"
+                  required
+                />
+              </Grid>
+              <Grid item xs={12} sm={12}>
+                <TextField
+                  fullWidth
+                  label="Mobile Number"
+                  name="mobileNumber"
+                  type="number"
+                  required
+                />
+              </Grid>
+              <Grid item xs={12} sm={12}>
+                <TextField
+                  fullWidth
+                  label="National ID"
+                  name="nationalId"
+                  type="number"
+                  required
+                />
+              </Grid>
+              <Grid item xs={12} sm={12}>
+                <TextField
+                  fullWidth
+                  label="Date of Birth"
+                  name="dateOfBirth"
+                  type="date"
+                  required
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={12}>
+                <TextField
+                  fullWidth
+                  label="Email"
+                  name="email"
+                  type="email"
+                  required
+                />
+              </Grid>
+              <Grid item xs={12} sm={12}>
+                <TextField
+                  fullWidth
+                  label="Password"
+                  name="password"
+                  type="password"
+                  required
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                >
+                  Sign Up
+                </Button>
+              </Grid>
+              <Grid item>
+                <Link to="/login">{"Already have an account? Sign In"}</Link>
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField fullWidth label="Last Name" name="lastName" required />
-            </Grid>
-            <Grid item xs={12} sm={12}>
-              <TextField
-                fullWidth
-                label="Mobile Number"
-                name="mobileNumber"
-                type="number"
-                required
-              />
-            </Grid>
-            <Grid item xs={12} sm={12}>
-              <TextField
-                fullWidth
-                label="National ID"
-                name="nationalId"
-                type="number"
-                required
-              />
-            </Grid>
-            <Grid item xs={12} sm={12}>
-              <TextField
-                fullWidth
-                label="Date of Birth"
-                name="dateOfBirth"
-                type="date"
-                required
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
-            </Grid>
-            <Grid item xs={12} sm={12}>
-              <TextField
-                fullWidth
-                label="Email"
-                name="email"
-                type="email"
-                required
-              />
-            </Grid>
-            <Grid item xs={12} sm={12}>
-              <TextField
-                fullWidth
-                label="Password"
-                name="password"
-                type="password"
-                required
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                fullWidth
-              >
-                Sign Up
-              </Button>
-            </Grid>
-            <Grid item>
-              <Link to="/login">{"Already have an account? Sign In"}</Link>
-            </Grid>
-          </Grid>
-        </form>
-      </Container>
-    </LoginContainer>
+            {error && (
+              <Alert severity="error" sx={{ mt: "20px" }}>
+                Something went wrong please try again later!
+              </Alert>
+            )}
+          </Box>
+        </Container>
+      </LoginContainer>
+    </>
   );
 };
