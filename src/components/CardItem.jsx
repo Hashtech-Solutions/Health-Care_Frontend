5dba585841b2803b395c779b023e1e4af1f5e6fa
MohammedAbdulaziz <mohamed.abaziz980@gmail.com>
2023-12-02 03:29:14 +0200

    Merge branch 'master' of https://github.com/Hashtech-Solutions/Veezeta-Frontend

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,27 +1,46 @@
-import * as React from 'react';
-import { Grid, Typography, Avatar, CardActionArea } from '@mui/material';
+import { Grid, Typography, CardActionArea } from "@mui/material";
 
 export const CardItem = (props) => {
   return (
     <Grid item xs={12} sm={12} md={6} lg={6}>
-      <CardActionArea onClick={() => console.log('Card clicked!')} sx={{
-        borderRadius: "20px", boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)", cursor: "pointer"
-      }}>
-        <Grid container
+      <CardActionArea
+        onClick={() => console.log("Card clicked!")}
+        sx={{
+          borderRadius: "20px",
+          boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+          cursor: "pointer",
+        }}
+      >
+        <Grid
+          container
           sx={{
-            backgroundColor: "white", borderRadius: "20px", height: "350px"
-          }}>
-          <Grid item xs={4} sm={4} md={4} lg={4} sx={{ padding: "40px 0 0 40px" }}>
-            <Avatar
-              alt="Profile Picture"
+            backgroundColor: "white",
+            borderRadius: "20px",
+            height: "350px",
+          }}
+        >
+          <Grid
+            item
+            xs={4}
+            sm={4}
+            md={4}
+            lg={4}
+            sx={{ padding: "40px 0 0 40px" }}
+          >
+            <img
+              alt="Profile"
               src="https://picsum.photos/200/300"
-              sx={{
-                width: { xs: '30vw', sm: '20vw', md: '20vw', lg: '10vw' },
-                height: { xs: '30vw', sm: '20vw', md: '20vw', lg: '10vw' },
-              }}
+              style={{ width: "120px", borderRadius: "50%", height: "120px" }}
             />
           </Grid>
-          <Grid item xs={8} sm={8} md={8} lg={8} sx={{ padding: "40px 0 0 40px" }}>
+          <Grid
+            item
+            xs={8}
+            sm={8}
+            md={8}
+            lg={8}
+            sx={{ padding: "40px 0 0 40px" }}
+          >
             {/* Column: Text Widgets */}
             <Grid container direction="column" spacing={2}>
               <Grid item>
