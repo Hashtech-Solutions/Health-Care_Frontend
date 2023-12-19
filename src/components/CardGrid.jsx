9312adeeb6f3d098702e9b7c4353a4f4eb282a8d
stevensamir20 <steven.samir@eng.suez.edu.eg>
2023-12-19 20:33:03 +0200

    ux updates

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,44 +1,129 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { BASE_URL } from "../shared/API";
+import { Categories } from "../shared/data/Categories";
+import { TestingDoctors } from "../shared/data/Testing";
 
 // Components
-import { Alert, Grid } from "@mui/material";
+import {
+  Grid,
+  MenuItem,
+  Select,
+  InputLabel,
+  FormControl,
+  TextField,
+  Alert,
+} from "@mui/material";
 import { ProfileCard } from "./ProfileCard";
-import { BackdropLoader } from "./BackdropLoader";
-import { TestingDoctors } from "../shared/data/Testing";
+import { StatusContainer } from "./StatusContainer";
 
 export const CardGrid = () => {
   const [doctors, setDoctors] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
+  const [status, setStatus] = useState("loading");
+  const [filteredDoctors, setFilteredDoctors] = useState([]);
+  const [search, setSearch] = useState("");
+  const [category, setCategory] = useState("");
 
   useEffect(() => {
     axios
       .get(`${BASE_URL}/doctor/all`)
       .then((res) => {
         setDoctors(res.data);
-        setLoading(false);
+        setFilteredDoctors(res.data);
+        setStatus(res.data.length > 0 ? "success" : "empty");
       })
       .catch((err) => {
-        // setError(true);
-        setLoading(false);
+        console.error(err);
+        setStatus("success");
+        setFilteredDoctors(TestingDoctors);
       });
   }, []);
 
+  useEffect(() => {
+    if (search === "") {
+      setFilteredDoctors(TestingDoctors);
+    } else {
+      const filtered = TestingDoctors.filter((doctor) => {
+        return (
+          doctor.firstName.toLowerCase().includes(search.toLowerCase()) ||
+          doctor.lastName.toLowerCase().includes(search.toLowerCase())
+        );
+      });
+      setFilteredDoctors(filtered);
+    }
+  }, [search, doctors]);
+
+  useEffect(() => {
+    if (category === "all") {
+      setSearch("");
+      setFilteredDoctors(TestingDoctors);
+    } else {
+      setSearch("");
+      const filtered = TestingDoctors.filter((doctor) => {
+        return doctor.specialization === category;
+      });
+      setFilteredDoctors(filtered);
+    }
+  }, [category, doctors]);
+
   return (
-    <Grid container spacing={2} sx={{ paddingTop: "50px" }}>
-      {loading ? (
-        <BackdropLoader />
-      ) : error ? (
-        <Alert severity="error" variant="filled">
-          Something went wrong, please try again later!
-        </Alert>
-      ) : (
-        TestingDoctors.map((doctor) => (
-          <ProfileCard isClickable={true} doctor={doctor} key={doctor.id} />
-        ))
-      )}
-    </Grid>
+    <>
+      <StatusContainer
+        status={status}
+        emptyMessage="There are no doctors found..."
+      >
+        <Grid
+          sx={{
+            display: "flex",
+            gap: "1%",
+          }}
+        >
+          <TextField
+            sx={{ width: "79%" }}
+            label="Search Doctors"
+            placeholder="Write a doctor name here..."
+            variant="outlined"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <FormControl
+            sx={{
+              width: "20%",
+            }}
+          >
+            <InputLabel>Specialization</InputLabel>
+            <Select
+              label="Specialization"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <MenuItem value="all">All Specializations</MenuItem>
+              {Categories.map((category) => (
+                <MenuItem value={category.name} key={category.id}>
+                  {category.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+
+        <Grid container spacing={2} sx={{ paddingTop: "30px" }}>
+          {filteredDoctors.length === 0 && (
+            <Alert
+              severity="info"
+              sx={{
+                width: "100%",
+                ml: 2,
+              }}
+            >
+              No doctors with that name or specialization were found!
+            </Alert>
+          )}
+          {filteredDoctors.map((doctor) => (
+            <ProfileCard isClickable={true} doctor={doctor} key={doctor.id} />
+          ))}
+        </Grid>
+      </StatusContainer>
+    </>
   );
 };
--- src/roles/patient/doctors/DoctorsView.jsx
+++ src/roles/patient/doctors/DoctorsView.jsx
@@ -1,38 +1,10 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { BASE_URL } from "../../../shared/API";
-
-// Components
 import { MainContainer } from "../../../components/MainContainer";
-import { StatusContainer } from "../../../components/StatusContainer";
 import { CardGrid } from "../../../components/CardGrid";
 
 export const DoctorsView = () => {
-  const [doctors, setDoctors] = useState([]);
-  const [status, setStatus] = useState("loading");
-
-  useEffect(() => {
-    axios
-      .get(`${BASE_URL}/doctor/all`)
-      .then((res) => {
-        console.log(res);
-        setDoctors(res.data);
-        setStatus(res.data.length > 0 ? "success" : "empty");
-      })
-      .catch((error) => {
-        console.error(error);
-        setStatus("error");
-      });
-  }, []);
-
   return (
     <MainContainer title="Doctors">
-      <StatusContainer
-        status={status}
-        emptyMessage="There are no doctors found..."
-      >
-        <CardGrid doctors={doctors} />
-      </StatusContainer>
+      <CardGrid />
     </MainContainer>
   );
 };
