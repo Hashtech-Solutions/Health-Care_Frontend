24ea5207a1a180ac965915bcc1317f413b513e05
stevensamir20 <steven.samir@eng.suez.edu.eg>
2023-12-21 17:58:57 +0200

    removing testing data

--- src/shared/data/Days.js
+++
@@ -1,9 +0,0 @@
-export const Days = [
-  { id: 1, name: "Saturday", value: "saturday" },
-  { id: 2, name: "Sunday", value: "sunday" },
-  { id: 3, name: "Monday", value: "monday" },
-  { id: 4, name: "Tuesday", value: "tuesday" },
-  { id: 5, name: "Wednesday", value: "wednesday" },
-  { id: 6, name: "Thursday", value: "thursday" },
-  { id: 7, name: "Friday", value: "friday" },
-];
